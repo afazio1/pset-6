042d2a2ba522f9707a7c4e7385e3f9810fb1a9e1
Alexa <alexa.fazio04@gmail.com>
2019-12-29 21:08:21 -0500

    got it working better

--- js/app.js
+++ js/app.js
@@ -3,48 +3,48 @@ window.onload = function() {
 
 	document.getElementById("button").onclick = submitForms;
 }
+let toDoList = [];
+let i = -1;
 
 const submitForms = function(e) {
-	//this needs work
 	e.preventDefault();
-	let toDoList = [];
+
+
+	i += 1;
+
+	console.log(k);
+	console.log(i);
+
 	const list = document.getElementById("ul");
+
+	let todo = {
+		title: document.getElementById("exampleFormControlInput1").value,
+		priority: document.getElementById("defaultCheck1").checked
+	}
+
+	toDoList.push(todo);
 	
-
-	const title = document.getElementById("exampleFormControlInput1").value;
-	const description = document.getElementById("exampleFormControlTextarea1").value;
-	const checkbox = document.getElementById("defaultCheck1").checked;
-
-	toDoList.push(title);
-	toDoList.push(description);
-	toDoList.push(checkbox); 
 	console.log(toDoList);
 	
 	const item = document.createElement("li");
-	for (let i = 0; i < 3; i++) {
-		const span = document.createElement("SPAN");
-		
-		if (i === 0) {
-			span.className = "title";
-			span.innerHTML = toDoList[i];
-		}
-		else if (i === 1) {
-			span.className = "description";
-			span.innerHTML = toDoList[i];
 
-		}
-		else if (toDoList[2] === true) {
-			span.className = "high-priority";
-			span.innerHTML = "!";
-		}
-		else if (toDoList[2] === false) {
-			span.className = "low-priority";
-			span.innerHTML = "!";
-		}
+	span = document.createElement("SPAN");
+	span.className = "title";
+	span.innerHTML = toDoList[i].title;
 
-		
-		item.appendChild(span);
+	if (toDoList[i].priority === true) {
+		span1 = document.createElement("SPAN");
+		span1.className = "high-priority";
+		span1.innerHTML = "!";
 	}
-	 list.append(item);
+	else if (toDoList[i].priority === false) {
+		span1 = document.createElement("SPAN");
+		span1.className = "low-priority";
+		span1.innerHTML = "!";
+	}
+
+	item.appendChild(span);
+	item.appendChild(span1);
+	list.append(item);
 	
 }
